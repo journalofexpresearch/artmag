8eba3c09803b4c211b93f4f7c6098a08b461ba7f
Journal Of Experiential Research <crkunferman@journalofexpresearch.com>
2025-12-23 16:00:43 -0800

    Create pages.php with header and footer includes
    Added basic structure for pages.php with header and footer includes.

---
+++ templates/pages.php
@@ -0,0 +1,14 @@
+<?php include_once('./inc/header.php');
+//  all header and nav are included in header
+//  Include other scripts from ./scripts/ here then add content of body below
+//  <body>
+?>
+
+
+
+<?php
+//  footer div and closing tags for entire document
+//  </body>
+// </html>
+include('./inc/footer.php');
+?>
